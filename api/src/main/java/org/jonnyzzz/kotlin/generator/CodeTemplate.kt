06f2d705e0e4cab2ebc58306de16a81db0b94303
Eugene Petrenko <eugene.petrenko@gmail.com>
2016-11-09 22:45:02 +0100

    more templates

--- api/src/main/java/org/jonnyzzz/kotlin/generator/CodeTemplate.kt
+++ api/src/main/java/org/jonnyzzz/kotlin/generator/CodeTemplate.kt
@@ -3,6 +3,13 @@ package org.jonnyzzz.kotlin.generator
 import java.text.SimpleDateFormat
 import java.util.*
 
+fun KotlinWriter.generateShortHeader() {
+  appendln("///////////////////////////////////////////")
+  appendln("/// THIS IS AUTO GENERATED FILE")
+  appendln("/// YOU MAY EDIT IT ON YOUR OWN RISK")
+  appendln("///////////////////////////////////////////")
+}
+
 fun KotlinWriter.generateFileHeader(name: String, version: String) {
   appendln("///////////////////////////////////////////")
   appendln("/// THIS IS AUTO GENERATED FILE")
